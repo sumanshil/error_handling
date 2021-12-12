006d26f5de07a6d81df12d8785d9f66e8a28e615
Suman Chandra Shil <sshil@pinterest.com>
2021-12-12 13:52:08 -0800

    Remove if condition

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,11 @@
-use std::{fs::File, io::Write};
+use std::{error::Error, fs::File, io::Write};
 
-fn main() {
-    write_hello("some_random_file")
+fn main() -> Result<(), Box<dyn Error + Sync + Send>>{
+    write_hello("some_random_file")?;
+    Ok(())
 }
 
-fn write_hello(file: &str) {
-    let r  = File::create(file);
-
-    if let Err(e) = r {
-        println!("hey there is some error creating file: {:?}", e);
-        return;
-    }
-    let mut f  = r.unwrap();
-
-    let r = f.write_all(b"Hello");
-
-    if let Err(e) = r {
-        println!("hey there is some error {:?}", e);
-    }
+fn write_hello(file: &str) -> Result<(), Box<dyn Error + Sync + Send>> {
+   File::create(file)?.write_all(b"Hello");
+    Ok(())
 }
