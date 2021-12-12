44eeddae10803115f64708cd6b389da25abe7817
Suman Chandra Shil <sshil@pinterest.com>
2021-12-12 13:46:14 -0800

    Remove nested if condition

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,16 @@ fn main() {
 
 fn write_hello(file: &str) {
     let r  = File::create(file);
-    if let Ok(mut f) = r {
-        let r = f.write_all(b"Hello");
-        if let Err(e) = r {
-            println!("hey there is some error {:?}", e);
-        }
+
+    if let Err(e) = r {
+        println!("hey there is some error creating file: {:?}", e);
+        return;
+    }
+    let mut f  = r.unwrap();
+
+    let r = f.write_all(b"Hello");
+
+    if let Err(e) = r {
+        println!("hey there is some error {:?}", e);
     }
 }
